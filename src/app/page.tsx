30fe0e2a8897350048d5b1e5f8242fa2965c48e6
Amil Ramesh Rubasinghe <53709351+AmilRubasinghe@users.noreply.github.com>
2024-02-13 00:24:40 +0530

    Change button action to server side action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,29 @@
 "use client"
-import Image from "next/image";
+
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
+
+import { displayText } from '../serverActions/serverAction'
 
 export default function Home() {
 
   const router = useRouter();
+
 
   interface user {
     name: string;
     id: number;
   }
 
-  const [isShow, setIsShow] = useState(false);
+
 
   const [userData, setUserData] = useState<user[]>([]);
 
-  const displayText = () => {
-    setIsShow(current => !current);
-  };
+  // async function displayText() {
+  //   "use server";
+  //   console.log('This is server action printed on server console')
+  // // return "Do Server Console Log - console.log('This is server action printed on server console')"
+  // };
 
   async function userslist() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -37,10 +42,8 @@ export default function Home() {
 
   return (
     <div className="grid place-items-center mt-10">
-      <button onClick={displayText} className="bg-[white] text-black p-5">Do Server Action </button>
-      {isShow && <div className="bg-[white] mt-5 text-black p-5">Do Server Console Log - console.log("This is server
-        action printed on server console")</div>}
-
+      <button onClick={() => displayText("This is server action printed on server console")} className="bg-[white] text-black p-5">Do Server Action </button>
+      {/* {isShow && <div className="bg-[white] mt-5 text-black p-5"></div>} */}
       <button onClick={userslist} className="bg-red-900 mt-8 text-white p-5">Get users data</button>
       <div className="mt-5">
         {userData && userData?.map((user: user, index) => {
@@ -51,7 +54,7 @@ export default function Home() {
       <form className="mt-3" onSubmit={onSubmit}>
         <input type="number" placeholder="Enter routing id" className="text-black" name="id" />
         <button type="submit"> Redirect dynamic route</button>
-      </form>
+      </form> 
 
     </div>
   );
